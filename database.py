2dc150221bc9c9b46f91912ef58786152724c62a
volchemist <pi@raspberry.(none)>
2015-07-11 15:43:32 +0200

    changed code presentation

--- database.py
+++ database.py
@@ -9,48 +9,48 @@ cache_size = 200
 
 
 def save_tick_data(tstamp, symbol, field, value):
-	observation = (tstamp, symbol, field, value)
-	cache.append(observation)
-	if len(cache) >= cache_size:
-		flush_cache()
+    observation = (tstamp, symbol, field, value)
+    cache.append(observation)
+    if len(cache) >= cache_size:
+        flush_cache()
 
 
 def flush_cache():
-	try:
-		con = lite.connect('../data/prices.sqlite')
-		cur = con.cursor()
-		cur.executemany("INSERT INTO intraday VALUES(?, ?, ?, ?)", cache)
-		con.commit()
-		del cache[:]
-	finally:
-		if con:
-			con.close()
+    try:
+        con = lite.connect('../data/prices.sqlite')
+        cur = con.cursor()
+        cur.executemany("INSERT INTO intraday VALUES(?, ?, ?, ?)", cache)
+        con.commit()
+        del cache[:]
+    finally:
+        if con:
+            con.close()
 
 
 def get_securities():
-	data = None
-	try:
-		con = lite.connect('../data/statics.sqlite')
-		con.row_factory = lite.Row
-		cur = con.cursor()
-		cur.execute('SELECT * FROM securities')
-		data = cur.fetchall()
-	finally:
-		if con:
-			con.close()
-	return data
+    data = None
+    try:
+        con = lite.connect('../data/statics.sqlite')
+        con.row_factory = lite.Row
+        cur = con.cursor()
+        cur.execute('SELECT * FROM securities')
+        data = cur.fetchall()
+    finally:
+        if con:
+            con.close()
+    return data
 
 
 def get_subscriptions():
-	data = None
-	try:
-		con = lite.connect('../data/statics.sqlite')
-		con.row_factory = lite.Row
-		cur = con.cursor()
-		cur.execute('SELECT * FROM subscriptions WHERE symbol NOT IN (SELECT symbol FROM securities WHERE expiry < datetime())')
-		data = cur.fetchall()
-	finally:
-		if con:
-			con.close()
-	return data
+    data = None
+    try:
+        con = lite.connect('../data/statics.sqlite')
+        con.row_factory = lite.Row
+        cur = con.cursor()
+        cur.execute('SELECT * FROM subscriptions WHERE symbol NOT IN (SELECT symbol FROM securities WHERE expiry < datetime())')
+        data = cur.fetchall()
+    finally:
+        if con:
+            con.close()
+    return data
 
